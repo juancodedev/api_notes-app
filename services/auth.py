2d2b6d4826036f601f9430f962a46a15a403d822
Juan Muñoz <cl.jmunoz@gmail.com>
2025-03-27 03:56:49 -0300

    add services to notes and auth

---
+++ services/auth.py
@@ -0,0 +1,26 @@
+from datetime import datetime, timedelta
+from jose import jwt
+from passlib.context import CryptContext
+from sqlalchemy.orm import Session
+from models import User
+import config
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def hash_password(password: str):
+    return pwd_context.hash(password)
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def create_access_token(data: dict, expires_delta: timedelta):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + expires_delta
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, config.SECRET_KEY, algorithm=config.ALGORITHM)
+
+def authenticate_user(db: Session, username: str, password: str):
+    user = db.query(User).filter(User.username == username).first()
+    if not user or not verify_password(password, user.password):
+        return None
+    return user
